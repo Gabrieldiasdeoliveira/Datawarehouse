4d2167e69f5c5ea4776a9d02eec5a0180efffef1
dias-codes <gabrielazor.gd@gmail.com>
2022-06-10 15:07:59 -0300

    Adicionar arquivos de projeto.

---
+++ Tabelas/Dim_Produto.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Dim_Produto]
+(
+	[Cod_Produto] NVARCHAR(50) NOT NULL PRIMARY KEY,
+	[Des_Produto] NVARCHAR(200) NULL,
+	[Art_Tamanho] NVARCHAR(200) NULL,
+	[Art_Sabor] NVARCHAR(200) NULL,
+	[Cod_Marca] NVARCHAR(50) NULL, 
+    CONSTRAINT [FK_Dim_Produto_Dim_Marca] FOREIGN KEY (Cod_Marca) REFERENCES [Dim_Marca]([Cod_Marca])
+
+)
